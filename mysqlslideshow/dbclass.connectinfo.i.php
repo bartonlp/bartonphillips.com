37bec219cbeec87dab0ddc40eaa9ae89d95ab222
Barton Phillips <bartonphillips@gmail.com>
2014-05-15 15:07:13 -0400

    Delete junk

--- mysqlslideshow/dbclass.connectinfo.i.php
+++
@@ -1,6 +0,0 @@
-<?php
-// This file can be place in a directory that is not in the Apache path for extra security.
-$Host = "localhost:3306"; // the host. This is probably OK unless you are using a remote host
-$User = "YourUserName"; // Change this to your MySql user name
-$Password = "YourPassword"; // Change this to you MySql password
-?>
