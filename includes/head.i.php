10d2406747342caa549cf15d311899d8a3f4d9d4
Barton Phillips <bartonphillips@gmail.com>
2021-03-26 13:58:49 -0400

    This directory is for my primary domain www.bartonphillips.com. I have consolidated a lot of thing so the other domains use some or all of the includes/ 	new file:   includes/README.md 	modified:   includes/banner.i.php 	modified:   includes/head.i.php

--- includes/head.i.php
+++ includes/head.i.php
@@ -1,14 +1,36 @@
 <?php
-// BLP 2018-05-11 -- ADD 'NO GOOD MSIE' message
-  
-if(!$this->noTrack) {
-  $trackerStr = '<script src="https://bartonphillips.net/js/tracker.js"></script>';
-} else {
+// BLP 2021-03-26 -- Added logic to not do tracker stuff if nodb or noTrack set
+
+if($this->noTrack === true || $this->nodb === true) {
   $trackerStr = '';
+} else {
+  $trackerStr =<<<EOF
+<script data-lastid="$this->LAST_ID" src="https://bartonphillips.net/js/tracker.js"></script>
+EOF;
+} 
+
+// BLP 2021-03-26 -- some sites may have a different defaultCss. If not set use our default
+
+if(!$this->defaultCss) {
+  $this->defaultCss = 'https://bartonphillips.net/css/blp.css';
 }
 
-if(!$arg['keywords']) {
-  $arg['keywords'] = "Barton Phillips, Programming, Weather Station, Tips and Tutorials, BLOG, About the Internet";
+// BLP 2021-03-26 -- default favicon
+
+if(!$this->favicon) {
+  $this->favicon = "https://bartonphillips.net/images/favicon.ico";
+}
+
+// BLP 2021-03-26 -- $arg takes president over $this from mysitemap.json
+
+if(empty($arg['keywords'])) {
+  $arg['keywords'] = $this->keywords;
+}
+if(empty($arg['title'])) {
+  $arg['title'] = $this->title;
+}
+if(empty($arg['desc'])) {
+  $arg['desc'] = $this->desc;
 }
 
 return <<<EOF
@@ -24,21 +46,15 @@ return <<<EOF
   <!-- More meta data -->
 {$arg['meta']}
   <!-- ICONS, RSS -->
-  <link rel="shortcut icon" href="https://bartonphillips.net/images/favicon.ico">
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="https://bartonphillips.net/css/blp.css" title="blp default">
-  <!-- css is not css but a link to tracker via .htaccess RewriteRule. -->
-  <link rel="stylesheet" href="/csstest-{$this->LAST_ID}.css" title="blp test">
+  <link rel="shortcut icon" href="$this->favicon">
+  <!-- default CSS -->
+  <link rel="stylesheet" href="$this->defaultCss" title="default">
 {$arg['link']}
   <!-- jQuery -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-migrate-3.3.2.min.js"></script>
-  <script>
-jQuery.migrateMute = false;
-jQuery.migrateTrace = false;
-  </script>
-  <script>var lastId = "$this->LAST_ID";</script>
-  $trackerStr
+  <script>jQuery.migrateMute = false; jQuery.migrateTrace = false;</script>
+$trackerStr
 {$arg['extra']}
 {$arg['script']}
 {$arg['css']}
