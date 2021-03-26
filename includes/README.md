531f74f03efbc199e88fae6746fc088f13c6ef98
Barton Phillips <bartonphillips@gmail.com>
2021-03-26 13:58:49 -0400

    This directory is for my primary domain www.bartonphillips.com. I have consolidated a lot of thing so the other domains use some or all of the includes/ 	new file:   includes/README.md 	modified:   includes/banner.i.php 	modified:   includes/head.i.php

---
+++ includes/README.md
@@ -0,0 +1,6 @@
+# Includes for the bartonphillips.com domain
+The head.i.php and footer.i.php files are used by most other of my domains. Some, like bartonlp.org (at www/html), 
+use the entire includes directory as a simlink while others just have head.i.php or footer.i.php symlinked.
+There is now logic in these file to use elements from the mysitemap.json file for things like defaultCss and favicon.
+
+
--- includes/banner.i.php
+++ includes/banner.i.php
@@ -1,16 +1,24 @@
 <?php
+// BLP 2021-03-26 -- add nodb logic
+
+if($this->nodb !== true && $this->noTrack !== true) {
+  $image2 =<<<EOF
+  <a>
+    <img id='linuxcounter' src="/tracker.php?page=normal&id=$this->LAST_ID" alt="linux counter image.">
+  </a>
+EOF;
+  $image3 = '<img src="tracker.php?page=noscript&id=$this->LAST_ID">';
+}
+
 return <<<EOF
 <header>
   <a href="http://www.bartonphillips.com">
     <img id='logo' src="https://bartonphillips.net/images/blp-image.png"></a>
-  <!-- the 'a' tag must be at the end of the image src otherwise we get an '-'-->
-  <a>
-    <img id='dummy' src="tracker.php?page=normal&id=$this->LAST_ID">
-  </a>
+$image2
 $mainTitle
 <noscript>
 <p style='color: red; background-color: #FFE4E1; padding: 10px'>
-<img src="tracker.php?page=noscript&id=$this->LAST_ID">
+$image3
 Your browser either does not support <b>JavaScripts</b> or you have JavaScripts disabled, in either case your browsing
 experience will be significantly impaired. If your browser supports JavaScripts but you have it disabled consider enabaling
 JavaScripts conditionally if your browser supports that. Sorry for the inconvienence.</p>
