f3ff632dbbf03500beac1e79aeb506b57f8db7e1
Barton Phillips <bartonphillips@gmail.com>
2021-04-01 06:54:37 -0400

    modified:   articles/buildawebsite.php 	new file:   articles/cssvariables.php 	modified:   index.php 	new file:   showmarkdown.php 	deleted:    spacestation.php

---
+++ articles/cssvariables.php
@@ -0,0 +1,142 @@
+<?php
+$_site = require_once(getenv("SITELOADNAME"));
+ErrorClass::setDevelopment(true);
+//$_site->noTrack = true;
+$S = new $_site->className($_site);
+
+$h->banner = "<h1>How to Use CSS Variables</h1>";
+$h->css =<<<EOF
+<style>
+body {
+  padding: 1rem;
+  font-family: sans-serif;
+}
+article {
+  padding: 2rem;
+  margin-bottom: 1rem;
+  cursor: pointer;
+  background: lightgray;
+  transition: 100ms;
+}
+article h2:hover {
+  background: var(--custome_color);
+  color: var(--color);
+}
+h2 {
+  font-size: 1.8rem;
+  margin-bottom: .5rem;
+}
+.my-element {
+  position: relative;
+  background: pink;
+  width: 500px;
+  height: 100px;
+}
+.my-element::after {
+  position: absolute;
+  content: var(--testit, none);
+  padding-left: 100px;
+  margin-left: 5px;
+  top: 1em;
+  left: 0px;
+  background: lightblue;
+}
+</style>
+EOF;
+
+$b->script =<<<EOF
+<script>
+  $(".my-element").on("click", function() {
+    $(this).css('--testit', '"hello world"');
+  });
+</script>
+EOF;
+
+$h->script =<<<EOF
+<script src="https://bartonphillips.net/js/syntaxhighlighter.js"></script>
+<link rel='stylesheet' href="https://bartonphillips.net/css/theme.css">
+EOF;
+
+[$top, $footer] = $S->getPageTopBottom($h,$b);
+
+$my_element1 =<<<EOF
+<style>
+.my-element {
+  position: relative;
+  background: pink;
+  width: 500px;
+  height: 100px;
+}
+.my-element::after {
+  position: absolute;
+  content: var(--testit, none);
+  padding-left: 100px;
+  margin-left: 5px;
+  top: 1em;
+  left: 0px;
+  background: lightblue;
+}
+</style>
+EOF;
+$my_element1 = escapeltgt($my_element1);
+
+$my_element2 =<<<EOF
+<script>
+  $(".my-element").on("click", function() {
+    $(this).css('--testit', '"hello world"');
+  });
+</script>
+EOF;
+$my_element2 = escapeltgt($my_element2);
+
+$other =<<<EOF
+<style>
+article {
+  padding: 2rem;
+  margin-bottom: 1rem;
+  cursor: pointer;
+  background: lightgray;
+  transition: 100ms;
+}
+article h2:hover {
+  background: var(--custome_color);
+  color: var(--color);
+}
+h2 {
+  font-size: 1.8rem;
+  margin-bottom: .5rem;
+}
+</style>
+
+<article>
+<h2 style='--custome_color: red;'>This is a test</h2>
+<h2 style='--custome_color: blue; --color: white;'>More test</h2>
+<div class='my-element'>This is without</div>
+</article>
+EOF;
+$other = escapeltgt($other);
+
+echo <<<EOF
+$top
+<p>
+This has a 'div' with a class of 'my-element', and 'my-element' has a '::after'  with 'content' of 'none'.
+Now I want to change the 'content' to 'hello world'. To do that I use a css vaiable;</p>
+<pre class="brush: css">
+$my_element1
+</pre>
+<p>Then with a little jQuery like this:</p>
+<pre class="brush: js">
+$my_element2
+</pre>
+<p>Now when you click the line that says This is without' you will see the 'hello world' in light blue.</p>
+<p>The two lines above 'This is without' are also done with a CSS variable (NO javascript) to get the hover effect.</p>
+<pre class="brush: html">
+$other
+</pre>
+<article>
+<h2 style='--custome_color: red;'>This is a test</h2>
+<h2 style='--custome_color: blue; --color: white;'>More test</h2>
+<div class='my-element'>This is without</div>
+</article>
+$footer
+EOF;
---
+++ showmarkdown.php
@@ -0,0 +1,92 @@
+<?php
+// BLP 2021-03-29 -- NON-PUG version
+  
+$_site = require_once(getenv("SITELOADNAME"));
+ErrorClass::setDevelopment(true);
+$S = new $_site->className($_site);
+
+$h->css =<<<EOF
+<style>
+input, select { font-size: 1rem; padding-left: .3rem; margin-bottom: 1rem;}
+input[type='text'] { width: 20rem; }
+</style>
+EOF;
+
+[$top, $footer] = $S->getPageTopBottom($h);
+
+if($_POST || ($get = $_GET['filename'])) {
+  if($get) {
+    $file = $get;
+    $type = "GitHub";
+  } else {
+    $file = $_POST['filename'];
+    $type = $_POST['type'];
+  }
+
+  switch($type) {
+    case "GitHub":
+      $parser = new \cebe\markdown\GithubMarkdown();
+      $github =<<<EOF
+  <link rel="stylesheet" href="https://bartonphillips.net/css/theme.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.js"></script>
+  <script src="https://bartonphillips.net/js/syntaxhighlighter.js"></script>
+  <script>
+jQuery(document).ready(function($) {
+  var code = $("code[class|='language'");
+  $(code).each(function(i, e) {
+    var cl = $(e).attr('class');
+    $(e).parent().addClass(cl.replace(/language-(.*)/, 'brush: $1'));
+    $(e).parent().html($(e).html());
+    $(e).remove();
+  });
+});
+  </script>
+EOF;
+      break;
+    case "Traditional":
+      $parser = new \cebe\markdown\Markdown();
+      break;
+    case "Extended":
+      $parser = new \cebe\markdown\MarkdownExtra();
+      break;
+    case "RAW":
+      $parser = "RAW";
+      break;
+    default:
+      echo "ERROR $type<br>";
+      exit();
+  }
+
+  //echo "file: $file<br>";
+  
+  $output = file_get_contents($file);
+
+  if(empty($output)) {
+    echo "<h1 style='text-align: center; font-size: 2rem;'>File Not Found<br>$file</h1>";
+    exit();
+  }
+
+  if($parser != "RAW") {
+    $output = $parser->parse($output);
+  }
+} else {
+  echo <<<EOF
+$top
+<form action='showmarkdown.php' method='post'>
+  <input type='text' name='filename' autofocus placeholder='Enter Markdown File Name' required>
+  <br>
+  <select name='type'>
+    <option>GitHub</option>
+    <option>Traditional</option>
+    <option>Extended</option>
+    <option>RAW</option>
+  </select>
+  <br>
+  <input type='submit' value='Submit'>
+</form>
+$footer
+EOF;
+}
+
+echo $output;
+
